20ac0fe91d9553063bc47be9380b99ead14ba136
ch-folio <chelhuds@gmail.com>
2023-10-02 15:07:20 -0700

    Made minor changes to the appearance of the game and its buttons

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 let playerText = document.getElementById('playerText')
 let restartBtn = document.getElementById('restartBtn')
-let boxes = Array.from(document.getElementsByClassName('box'))
+let boxes = Array.from(document.getElementsByClassName('box'));
 
 
 let playerChoice = prompt("Welcome to Tic Tac Toe! Choose 'X' or 'O'");
@@ -8,67 +8,106 @@ let playerChoice = prompt("Welcome to Tic Tac Toe! Choose 'X' or 'O'");
 // Validate user input
 while (playerChoice !== 'X' && playerChoice !== 'O') {
     playerChoice = prompt("Invalid choice! Please choose 'X' or 'O'");
-  }
+}
 
-let winnerIndicator = getComputedStyle(document.body).getPropertyValue('--winning-blocks')
+let winnerIndicator = getComputedStyle(document.body).getPropertyValue('--winning-blocks');
 
-const O_TEXT = "O"
-const X_TEXT = "X"
+const O_TEXT = "O";
+const X_TEXT = "X";
 let currentPlayer = playerChoice;
-let spaces = Array(9).fill(null)
+let spaces = Array(9).fill(null);
+
+let winningBlocks = null;
 
 const startGame = () => {
-    boxes.forEach(box => box.addEventListener('click', boxClicked))
-}
+    boxes.forEach((box) => box.addEventListener('click', boxClicked));
+};
 
 function boxClicked(e) {
-    const id = e.target.id
+    const id = e.target.id;
 
-    if(!spaces[id]){
-        spaces[id] = currentPlayer
-        e.target.innerText = currentPlayer
+    if (!spaces[id]) {
+        spaces[id] = currentPlayer;
+        e.target.innerText = currentPlayer;
 
-        if(playerHasWon() !==false){
-            playerText.innerHTML = `${currentPlayer} has won!`
-            let winning_blocks = playerHasWon()
-
-            winning_blocks.map( box => boxes[box].style.backgroundColor=winnerIndicator)
-            return
+        if (playerHasWon() !== false) {
+            playerText.innerHTML = `${currentPlayer} has won!`;
+            let winning_blocks = playerHasWon();
+            
+            winning_blocks.forEach((box) => (boxes[box].style.backgroundColor = winnerIndicator));
+            return;
         }
 
-        currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT
+        currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT;
 
         if (currentPlayer == X_TEXT || currentPlayer == O_TEXT) {
-            computerTurn() // Computer plays after the user square is chosen
-          }
-    }
-}
-
-//Computer selects a random, empty space
-function computerTurn() {
-    const emptySpaces = spaces.reduce((acc, elem, index) => {
-        if (elem == null) {
-            acc.push(index)
+            computerTurn();
         }
-        return acc
-    }, [])
-    
-    const randomIndex = Math.floor(Math.random() * emptySpaces.length)
-    const computerMove = emptySpaces[randomIndex]
-    
-    spaces[computerMove] = currentPlayer
-    boxes[computerMove].innerText = currentPlayer
-    
-    if(playerHasWon() !== false){
-        playerText.innerHTML = `${currentPlayer} has won!`
-        let winning_blocks = playerHasWon()
+    }
+}
 
-        winning_blocks.map( box => boxes[box].style.backgroundColor=winnerIndicator)
-        return
+//Computer chooses any empty space on the board
+function computerTurn() {
+    if (!playerHasWon()) {
+        const emptySpaces = spaces.reduce((acc, elem, index) => {
+            if (elem == null) {
+                acc.push(index);
+            }
+            return acc;
+        }, []);
+        
+        const randomIndex = Math.floor(Math.random() * emptySpaces.length);
+        const computerMove = emptySpaces[randomIndex];
+        
+        spaces[computerMove] = currentPlayer;
+        boxes[computerMove].innerText = currentPlayer;
+        
+        if(playerHasWon()) {
+            playerText.innerHTML = `${currentPlayer} has won!`;
+            winningBlocks.forEach((box) => (boxes[box].style.backgroundColor = winnerIndicator));
+            return;
+        }
+    
+        currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT;
+    }
+}
+
+//Computer always tries to choose a space adjacent to a space it already owns
+function computerTurnIntermediate() {
+    const ownedSpaces = spaces.reduce((acc, elem, index) => { if (elem === currentPlayer) { acc.push(index); } return acc; }, []);
+    const adjacentSpaces = ownedSpaces.reduce((acc, elem) => { const adjacent = [elem - 1, elem + 1, elem - gridSize, elem + gridSize]; adjacent.forEach((space) => { if (spaces[space] === null && !acc.includes(space) && space >= 0 && space < spaces.length) { acc.push(space); } }); return acc; }, []);
+    const randomIndex = Math.floor(Math.random() * adjacentSpaces.length); const computerMove = adjacentSpaces[randomIndex];
+    
+    spaces[computerMove] = currentPlayer; boxes[computerMove].innerText = currentPlayer;
+
+    if (playerHasWon() !== false) { 
+        playerText.innerHTML = `${currentPlayer} has won!`;
+        let winning_blocks = playerHasWon();
+        winning_blocks.forEach((box) => (boxes[box].style.backgroundColor = winnerIndicator)); return;
     }
 
-    currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT
+currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT;
 }
+
+//Allow user to select the beginner level and validate with a green checkmark upon click
+document.getElementById('beginner').addEventListener('click', function() {
+    this.classList.toggle('checked');
+    document.getElementById('intermediate').classList.remove('checked');
+});
+
+//Allow user to select the intermediate level and validate with a green checkmark upon click
+document.getElementById('intermediate').addEventListener('click', function() {
+    this.classList.toggle('checked');
+    document.getElementById('beginner').classList.remove('checked');
+});
+
+//Logic for game difficulty selection that is not currently working
+
+/* const beginnerButton = document.getElementById("beginner");
+const intermediateButton = document.getElementById("intermediate");
+
+beginnerButton.addEventListener("click", computerTurn);
+intermediateButton.addEventListener("click", computerTurnIntermediate); */
 
 const winningCombos = [
     [0,1,2],
@@ -79,32 +118,43 @@ const winningCombos = [
     [2,5,8],
     [0,4,8],
     [2,4,6]
-]
+];
 
 function playerHasWon() {
     for (const condition of winningCombos) {
-        let [a, b, c] = condition
+        let [a, b, c] = condition;
 
         if(spaces[a] && (spaces[a] == spaces[b] && spaces[a] == spaces[c])) {
-            return [a,b,c]
+            winningBlocks = [a, b, c];
+            return true;
         }
     }
-    return false
+    return false;
 }
 
-restartBtn.addEventListener('click', restart)
+restartBtn.addEventListener('click', restart);
 
 function restart() {
-    spaces.fill(null)
+    spaces.fill(null);
+    winningBlocks = null;
 
-    boxes.forEach( box => {
-        box.innerText = ''
-        box.style.backgroundColor=''
-    })
+    boxes.forEach((box) => {
+        box.innerText = '';
+        box.style.backgroundColor = '';
+    });
 
-    playerText.innerHTML = 'Tic Tac Toe'
+    let newPlayerChoice = prompt("Choose 'X' or 'O'");
+    while (newPlayerChoice.toUpperCase() !== 'X' && newPlayerChoice.toUpperCase() !== 'O') {
+        newPlayerChoice = prompt("Invalid choice! Please choose 'X' or 'O'");
+    }
+    
+    // Update currentPlayer based on the new player choice
+    currentPlayer = newPlayerChoice.toUpperCase() === 'X' ? X_TEXT : O_TEXT;
+    playerChoice = newPlayerChoice.toUpperCase(); // Assign the new player choice to playerChoice variable
 
-    currentPlayer = playerChoice
+    playerText.innerHTML = 'Tic Tac Toe';
 }
 
-startGame()
+startGame();
+
+
